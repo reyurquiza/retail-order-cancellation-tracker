9404c63dce7c4283dca297a885971be328bb9901
Rey Urquiza <reiiurquiza@gmail.com>
2025-08-08 15:30:35 -0700

    Updated to reflect cache handling change.

--- config.py
+++ config.py
@@ -1,18 +1,7 @@
 # Multiple email accounts
-# Must use app password not account password
-EMAIL_ACCOUNTS = [
-    {
-        "email": "",
-        "password": "",
-        "imap_server": "imap.gmail.com"
-    },
-    {
-        "email": "",
-        "password": "",
-        "imap_server": "imap.gmail.com"
-    }
-]
+EMAIL_ACCOUNTS = []
 
-DAYS_BACK = 1
-CSV_PATH = "output/report.csv"
-CACHE_JSON = "cache/emails.json"
+DAYS_BACK = 7
+OUTPUT_DIR = ""
+CSV_PATH = ""
+CACHE_JSON = ""
